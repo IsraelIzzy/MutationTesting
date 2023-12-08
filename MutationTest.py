d9d12697c472c86d18a8896afc925b7752c747f0
IsraelIzzy <147438501+IsraelIzzy@users.noreply.github.com>
2023-12-08 10:57:30 -0500

    Update MutationTest.py

--- MutationTest.py
+++ MutationTest.py
@@ -23,7 +23,7 @@ def test_str():
     poly3 = polynomial([0, 0, 0])
     assert str(poly3) == "0" or str(poly3) == ""
 
-    poly = polynomial([1, 0, -3])  # coefficients: [1, 0, -3]
+    poly = polynomial([1, 0, -3])  
     result = str(poly)
     assert result == "1x^2 + -3"
 
@@ -34,11 +34,11 @@ def test_str():
     assert str(poly5) == "0"
 
 def test_add():
-    # Test addition with polynomials of the same length
-    poly1 = polynomial([1, 2, 3])  # coefficients: [1, 2, 3]
-    poly2 = polynomial([4, 5, 6])  # coefficients: [4, 5, 6]
+   
+    poly1 = polynomial([1, 2, 3])  
+    poly2 = polynomial([4, 5, 6])  
     result = poly1 + poly2
-    assert result.coefficients == [5, 7, 9]  # Check the resulting coefficients
+    assert result.coefficients == [5, 7, 9]  
 
     poly1 = polynomial([3, 0, 2])
     poly2 = polynomial([1, -1])
@@ -50,19 +50,19 @@ def test_add():
     poly_sum = poly1 + poly2
     assert poly_sum.coefficients == [3, 0, 2]
 
-    # Test addition with both empty polynomials
-    poly1 = polynomial([])  # coefficients: []
-    poly2 = polynomial([])  # coefficients: []
+    
+    poly1 = polynomial([])  
+    poly2 = polynomial([])  
     result = poly1 + poly2
-    assert result.coefficients == []  # Check the resulting coefficients
+    assert result.coefficients == []  
 
-    # Test addition with other polynomial having length None
-    poly1 = polynomial([1, 2, 3])  # coefficients: [1, 2, 3]
-    poly2 = polynomial(None)  # coefficients: None
+    
+    poly1 = polynomial([1, 2, 3])  
+    poly2 = polynomial(None)  
     result = poly1 + poly2
-    # Expected behavior could vary based on your implementation,
-    # for example, handling None as an empty polynomial.
-    assert result.coefficients == [1, 2, 3]  # Check the resulting coefficients
+    
+
+    assert result.coefficients == [1, 2, 3]  
 def test_sub():
     poly1 = polynomial([3, 0, 2])
     poly2 = polynomial([1, -1])
@@ -71,25 +71,25 @@ def test_sub():
     assert poly_diff.coefficients == [3,-1, 3]
 
 def test_mul():
-    # Test multiplication with two empty polynomials
+
     poly1 = polynomial([])
     poly2 = polynomial([])
     result = poly1 * poly2
     assert result.coefficients == []
 
-    # Test multiplication with a zero polynomial and another polynomial
+    
     poly1 = polynomial([0])
     poly2 = polynomial([3, 3, 3])
     result = poly1 * poly2
     assert result.coefficients == [0, 0, 0]
 
-    # Test multiplication with identity polynomial and another polynomial
+   
     poly1 = polynomial([1])
     poly2 = polynomial([4, 5, 6])
     result = poly1 * poly2
     assert result.coefficients == [4, 5, 6]
 
-    # Test multiplication with arbitrary polynomials
+  
     poly1 = polynomial([5, 4, 3])
     poly2 = polynomial([4, 9])
     result = poly1 * poly2
@@ -97,7 +97,7 @@ def test_mul():
 
 def test_evaluate():
 
-    # Test evaluation of an empty polynomial
+    
     poly = polynomial([])
-    result = poly.evaluate(10)  # Evaluate at x = 5 (arbitrary value)
-    assert result == 0  # Expected result when evaluating an empty polynomial is 0
+    result = poly.evaluate(10)  
+    assert result == 0 
